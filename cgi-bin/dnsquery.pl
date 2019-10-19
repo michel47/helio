160a8c36764714e1dcc1ccdac9d700da3e30fa2e
Ali C. Seki <aseki@healthium.gq>
2019-10-19 23:04:20 +0200

    again some more typos

--- cgi-bin/dnsquery.pl
+++ cgi-bin/dnsquery.pl
@@ -31,7 +31,7 @@ use if -e '/usr/local/share/perl5/cPanelUserConfig.pm', cPanelUserConfig;
 if (-e '/usr/local/share/perl5/cPanelUserConfig.pm') {
   use lib $ENV{HOME}.'/repositories';
 } else {
-  use lib $ENV{SITE}.'/lib' if (exist $ENV{SITE});
+  use lib $ENV{SITE}.'/lib';
 }
 use Brewed::DNS qw(get_rrecord);
 # A : 1, NS : 2, MD : 3, MF : 4, CNAME : 5, SOA : 6,

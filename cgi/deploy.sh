b1fedc7584e9863db641b600422a6f2e15807d77
Mary G. Auala <mauala@healthium.gq>
2020-01-27 16:27:57 +0100

    adding a dynsm site for dynamic function for static sites (hostless)

---
+++ cgi/deploy.sh
@@ -0,0 +1,29 @@
+#
+
+# https://iph.heliohost.org/cgi/testing.pl/cmd.pl
+set -e
+set -x
+
+# getting a clean "checkout"
+cd ~/repositories/helio
+# reset see [*](https://www.ocpsoft.org/tutorials/git/reset-and-sync-local-respository-with-remote-branch/)
+if false; then
+git fetch origin
+git reset --hard origin/master
+git clean -f -d
+fi
+
+git status
+git pull
+
+# deploy : i.e. mirroring to public_html ...
+cd ~/public_html
+rsync -avuz ../repositories/helio/index.html .
+rsync -avuz ../repositories/helio/style.css .
+rsync -avuz ../repositories/helio/cgi-bin/ cgi-bin
+rsync -avuz ../repositories/helio/cgi/ cgi
+#rm cgi-bin
+#ln -s /home/michelc/repositories/helio/cgi-bin cgi-bin
+
+ls -l cgi-bin
+
